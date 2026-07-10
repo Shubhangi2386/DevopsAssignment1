bde959119ca4ad39aab6012472710706980a26e4
Shubhangi2386 <shubhangi.a.rathod@gmail.com>
2026-07-10 13:20:41 +0530

    Add project files.

---
+++ DevopsAssignment1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevopsAssignment1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
